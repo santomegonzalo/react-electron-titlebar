bd8733121dfdf22e0fe6618ca56aac8758f77d23
Gonzalo Santomé <gonzalo.santome@farfetch.com>
2018-12-05 16:26:59 +0000

    removing unnecesary files

--- example/build-scripts/build-mac.sh
+++
@@ -1,5 +0,0 @@
-export CSC_LINK="$(pwd)/mac-cert.p12"
-export WIN_CSC_LINK="$(pwd)/win-cert.pfx"
-export CSC_KEY_PASSWORD=""
-
-npm run electron-pack
--- example/build-scripts/build-win.cmd
+++
@@ -1,4 +0,0 @@
-set CSC_LINK=%cd%\build-assets\win-cert.pfx
-set CSC_KEY_PASSWORD=password
-
-call npm run package
